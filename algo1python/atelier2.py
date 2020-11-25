ac00cf3cf872dd7ee942d1f61468c83150f4e3d5
etica <etica>
2020-11-25 15:26:45 +0100

    commentaires

--- algo1python/atelier2.py
+++ algo1python/atelier2.py
@@ -13,12 +13,14 @@ def regroupe_1(tab):
     part1 = 0
     part3 = len(tab) - 1
 
-    # si la valeur à l'index i est < 0, il échange avec la v
+    # si la valeur à l'index i est < 0, échange avec part1 et incrémente part1
+    # sinon, si valeur à i > 0 , échange avec part3 et décrémente part3
+    # les 0 se retrouvent au centre
     for i in range(len(tab)):
         if tab[i] < 0:
             tab[part1], tab[i] = tab[i], tab[part1]
             part1 += 1
-            
+
         elif tab[i] > 0 and i < part3:
             tab[part3], tab[i] = tab[i], tab[part3]
             part3 -= 1
