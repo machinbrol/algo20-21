7ea631c79e33266cbac86bed2eea09240fa25449
etica <etica>
2020-11-25 09:43:45 +0100

    Suppression types hints

--- algo1python/atelier1.py
+++ algo1python/atelier1.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from typing import *
 from random import randrange
 from time import time
 
 
-# Supprime élément d'une liste à l'index pos
-def sup_at_pos(lst: List[int], pos: int) -> List[int]:
+# Supprime élément de liste 'lst' à l'index 'pos'
+def sup_at_pos(lst, pos):
     # len(lst) - 1 pcq qu'on get pos + 1
     while pos < len(lst) - 1:
         lst[pos] = lst[pos + 1]
@@ -17,8 +16,7 @@ def sup_at_pos(lst: List[int], pos: int) -> List[int]:
 
 
 # Supprime toutes les valeurs val de la liste et retourne une liste rétrécie
-# complexité O(n) ? (dépend de manière linéaire du nombre d'éléments dans la liste)
-def sup_all_val(lst: List[int], val: int) -> List[int]:
+def sup_all_val(lst, val):
     # un idx d'insertion et un idx de lecture
     ins_idx = 0
     lect_idx = 0
@@ -33,9 +31,8 @@ def sup_all_val(lst: List[int], val: int) -> List[int]:
     return lst[:ins_idx]
 
 
-# crée une liste de taille `size` comprenant environ p% de valeurs `val`
-def rand_list(size=10, p=50, val=0) -> List[int]:
-    val_range = 10
+# crée une liste d'éléments aléatoires de taille `size` comprenant environ p% de valeurs `val`
+def rand_list(size=10, p=50, val=0, val_range = 10):
     lst = list()
 
     for i in range(size):
@@ -51,12 +48,12 @@ def rand_list(size=10, p=50, val=0) -> List[int]:
 
 
 # effectue `nbr_runs` de 'sup_all_val' sur liste de taille `lst_size` et retourne le temps total en milisecondes
-def time_sup_all_val(lst_size: int, nbr_runs: int = 1) -> float:
+def time_sup_all_val(lst_size, nbr_runs=1):
     tot_time = 0
     lst = rand_list(lst_size)
 
     for i in range(nbr_runs):
-        test_lst = lst.copy()
+        test_lst = lst[:]
 
         t0 = time()
         sup_all_val(test_lst, 0)
@@ -71,23 +68,15 @@ def time_sup_all_val(lst_size: int, nbr_runs: int = 1) -> float:
 def time_sup_all_val_all_sizes(sizes = [10000, 20000, 40000, 80000], nbr_runs = 4):
     for n in range(nbr_runs):
         print(f"--- run {n} ---")
+
         for size in sizes:
             time_ms = time_sup_all_val(size)
             print(f"{size}: {time_ms} milliseconds")
+
         print()
     
 
 def main():
-    # lst = [1, 4, 2, 3, 4, 4, 5, 6, 4]
-    # print(lst)
-    # lst = sup_at_pos(lst, 2)
-    # print(lst)
-
-    # lst = sup_all_val(lst, 4)
-    # print(lst)
-
-    # rand_list()
-
     time_sup_all_val_all_sizes()
 
 
