d81f47eb0f0f690086887217be69be9168850cb1
etica <etica>
2020-11-28 11:22:48 +0100

    deuxième version de partition et tests

--- algo1python/atelier2.py
+++ algo1python/atelier2.py
@@ -4,10 +4,20 @@
 from random import randrange
 from time import time
 
+
 def swap(ls, a, b):
     ls[a], ls[b] = ls[b], ls[a]
 
-#  fonction qui regroupe les éléments d’un lsleau composé exclusivement des entiers -1, 0 et 1 
+
+# retourne une liste de taille 'size' composée de -1, 0 ou 1 distribués de manière aléatoire
+def get_list(size):
+    lst = []
+    for i in range(size):
+        lst.append(randrange(1,4) - 2)
+    return lst
+
+
+#  fonction qui regroupe les éléments d’un tableau composé exclusivement des entiers -1, 0 et 1 
 #  de façon à ce que tous les -1 soient au début, suivis des 0 et, finalement des 1
 def regroupe_1(ls):
     negs = 0
@@ -30,7 +40,7 @@ def regroupe_1(ls):
 
 
 
-# Ecrivez une fonction qui regroupe les éléments d’un lsleau d’entiers de façon à ce que tous les négatifs 
+# Ecrivez une fonction qui regroupe les éléments d’un tableau d’entiers de façon à ce que tous les négatifs 
 # soient au début (dans un ordre quelconque), suivis des 0 et, finalement des positifs
 def regroupe_2(ls):
     # trois partitions
@@ -38,22 +48,20 @@ def regroupe_2(ls):
     p1 = 0
     p3 = len(ls) - 1
 
-    gauche = lambda x: x < 0
-    droite = lambda x: x > 0
-    # Si la valeur à l'index i est < 0, échange avec p1 et on incrément p1
-    #  sauf si i == p1,
-    #    alors on ne swap pas (inutile), on incrémente i et p1
-    # sinon, si valeur à i > 0 , échange avec p3 et décrément p3
-    # si c'est ni l'un ni l'autre (0), on avance
+    # Si la valeur à idx i > 0 , échange avec p3 et décrémente p3
+    # Sinon, si la valeur à l'index i est < 0, échange avec p1 et on incrément p1
+    #  SAUF si i == p1,
+    #    dans ce cas on ne swap pas (inutile), on incrémente p1 ET i
+    # Sinon, c'est ni l'un ni l'autre (0), on incrémente i
     i = 0
     while i <= p3:
         val = ls[i]
 
-        if droite(val):
+        if val > 0:
             swap(ls, i, p3)
             p3 -= 1
 
-        elif gauche(val):
+        elif val < 0:
             if i == p1: # dans ce cas, le négatif est déjà à sa place, pas de swap mais on avance i ET p1
                 i += 1
             else:
@@ -63,29 +71,97 @@ def regroupe_2(ls):
         else: # on a un élément du milieu, on avance i
             i += 1
 
+# ---- version 2, moins 'tricky'  -----
+
+# bi-partition autour d'un pivot
+# si plus grand que pivot, swap à droite et incrémente partition
+# sinon avance
+def part2(ls, end, pivot):
+    p = end # idx d'écriture
+    i = 0 # idx de lecture
+
+    while i <= p:
+        if ls[i] > pivot:
+            swap(ls, i, p)
+            p -= 1
+        else:
+            i += 1
+    return p
+
+# deux bi partitions, d'abord les positifs à droite
+# puis dans sous-liste 0 à droite
+def regroupe_3(ls):
+    p =  len(ls)-1
+
+    p = part2(ls, p, 0) # > 0 à droite
+    part2(ls, p, -1) # > -1 à droite
+
+
+# ----- mesure de la complexité moyenne des deux fonctions -----
+
+def test_regroup(regroup, lsts):
+    
+    for name, lst in lsts:
+        l = lst[:]
+
+        t0 = time()
+        regroup(l)
+        t = (time() - t0) * 1000
+
+        print(name, t)
+
+def compare_2_et_3():
+    l1 = get_list(10000)
+    l2 = get_list(20000)
+    l4 = get_list(40000)
+    l8 = get_list(80000)
+
+    lists = [("l1", l1), ("l2", l2), ("l4", l4), ("l8", l8)]
+
+    print("----- regroup_2 -------")
+    test_regroup(regroupe_2, lists)
+    print()
+    print("----- regroup3 -------")
+    test_regroup(regroupe_3, lists)
+
 
 if __name__ == "__main__":
-    ls = [0,-1,0,0,1,1,-1,0,-1,-1,-1,-1,1]
-    ls = [0,-1,0,0,1,1,-1,0,-1,-1,-1,-1,0]
-    ls = [0,-1,0,0,0,0,-1,0,-1,-1,-1,-1,0]
-    ls = [0,-1,0,0,1,1,-1,0,-1,-1,0,0,0]
-    #ls = [-1,-1,0,0,1,1,-1,0,-1,-1,0,0,0]
+    print("test regroup1")
     ls = [1, 0, -1, -1, 0, 1, 1, 0, -1, 1, 0, 1, -1]
 
     print(ls)
     regroupe_1(ls)
     print(ls)
 
+    l1 = [0, -7, 0, 0, 5, 9, -4, 0, -2, -9, -4, -8, 2]
+    l2 = [-1, -1, 0, 0, -1, -1, 0, 1, 1, 0, -1, 1, 0, 1, -1]
+    l3 = [8, 0, -2, -3, 0, 4, 5, 0, -1, 11, 0, 9, 10]
+    l4 = [8, -1, -2, -3, 1, 4, 5, -2, -1, 11, 2, 9, -10]
+    l5 = [-5, -1, -2, -3, -1, -4]
+
+    lists = [l1, l2, l3 ,l4, l5]
+
     print()
-    ls = [0,-7,0,0,5,9,-4,0,-2,-9,-4,-8,2]
-    ls = [1, 0, -1, -1, 0, 1, 1, 0, -1, 1, 0, 1, -1]
-    ls = [8, 0, -2, -3, 0, 4, 5, 0, -1, 11, 0, 9, -10]
-    # ls = [8, -1, -2, -3, 1, 4, 5, -2, -1, 11, 2, 9, -10]
-    # ls = [-5, -1, -2, -3, 1, 4, 5, -2, -1, 11, 2, 9, -10]
 
-    print(ls)
-    regroupe_2(ls)
-    print(ls)
+    print("test regroup2")
+    for lst in lists:
+        ls = lst[:]
+        print(ls)
+        regroupe_2(ls)
+        print(ls)
+        print()
+
+    print()
+    print("test regroup 3")
+    for lst in lists:
+        ls = lst[:]
+        print(ls)
+        regroupe_3(ls)
+        print(ls)
+        print()
+    
+    print()
+    compare_2_et_3()
 
 
-
+    
