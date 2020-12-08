742edc8cc4b223aa08bf6050a8769025ec3f4778
etica <etica>
2020-12-08 20:24:00 +0100

    atelier3

--- atelier3.py
+++ atelier3.py
@@ -2,25 +2,26 @@
 # -*- coding: utf-8 -*-
 
 
-def fusion(ls1, ls2):
+def fusion(lst1, lst2):
     res = list()
     i = 0
     j = 0
 
-    while i < len(ls1) and j < len(ls2):
-        if ls1[i] < ls2[j]:
-            res.append(ls1[i])
+    while i < len(lst1) and j < len(lst2):
+        if lst1[i] < lst2[j]:
+            res.append(lst1[i])
             i += 1
         else:
-            res.append(ls2[j])
+            res.append(lst2[j])
             j += 1
         
-    if i == len(ls1):
-        res.extend(ls2[j:])
+    if i == len(lst1):
+        res.extend(lst2[j:])
     else:
-        res.extend(ls1[i:])
+        res.extend(lst1[i:])
     
     return res
+
 
 if __name__ == "__main__":
     print(fusion([1,2,4,4,8], [3,5,6,9]))
